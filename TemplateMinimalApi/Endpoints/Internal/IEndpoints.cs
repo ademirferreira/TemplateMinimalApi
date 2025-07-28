711a5f8d0c0d870af04bbdfcf77560a5cd76c8b0
Ademir Ferreira <whoisademir@gmail.com>
2025-07-28 15:29:43 -0300

    Adição do template de endpoints para minimal api

---
+++ TemplateMinimalApi/Endpoints/Internal/IEndpoints.cs
@@ -0,0 +1,7 @@
+﻿namespace TemplateMinimalApi.Endpoints.Internal;
+
+public interface IEndpoints
+{
+    public static abstract void DefineEndpoints(IEndpointRouteBuilder app);
+    public static abstract void AddServices(IServiceCollection services, IConfiguration configuration);
+}
---
+++ TemplateMinimalApi/Endpoints/WeatherForecastEnpoints.cs
@@ -0,0 +1,43 @@
+﻿using TemplateMinimalApi.Endpoints.Internal;
+using TemplateMinimalApi.Models;
+
+namespace TemplateMinimalApi.Endpoints;
+
+public class WeatherForecastEnpoints : IEndpoints
+{
+    private const string BaseRoute = "api/weatherforecast";
+    private const string Tag = "WeatherForecast";
+
+    public static void DefineEndpoints(IEndpointRouteBuilder app)
+    {
+        app.MapGet($"{BaseRoute}", GetWeatherForecast)
+            .WithName("GetWeatherForecast")
+            .WithTags(Tag)
+            .Produces<IEnumerable<WeatherForecast>>(StatusCodes.Status200OK)
+            .WithOpenApi()
+            .WithTags(Tag)
+            .AllowAnonymous();
+    }
+
+
+    internal static IResult GetWeatherForecast()
+    {
+        var summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+        var forecast = Enumerable.Range(1, 5).Select(index =>
+            new WeatherForecast
+            (
+                DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                Random.Shared.Next(-20, 55),
+                summaries[Random.Shared.Next(summaries.Length)]
+            )).ToArray();
+        return Results.Ok(forecast);
+    }
+
+    public static void AddServices(IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddHttpClient();
+    }
+}
